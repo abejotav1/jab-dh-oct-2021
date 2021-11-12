5533b8b38b105464d515142dcce1d892dfe76658
abejotav1 <abejotav1@hotmail.com>
2021-11-11 19:16:50 -0600

    11/11/2021

--- app.js
+++ app.js
@@ -11,3 +11,9 @@ app.listen(port,() => console.log(`Servidor corriendo en puerto ${port}`));
 app.get('/',(req,res)=> {
     res.sendFile(path.resolve(__dirname,'./views/home.html'));
 })
+app.get('/register',(req,res)=> {
+    res.sendFile(path.resolve(__dirname,'./views/register.html'));
+})
+app.get('/login',(req,res)=> {
+    res.sendFile(path.resolve(__dirname,'./views/login.html'));
+})
