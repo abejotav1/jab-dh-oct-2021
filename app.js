b6a88787662324b57d81b440872799cf35865b93
abejotav1 <abejotav1@hotmail.com>
2021-11-18 23:03:19 -0600

    Configurando Procfile

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 const publicPath = path.resolve(__dirname, './public');
 app.use (express.static(publicPath));
 
-app.listen(port,() => console.log(`Servidor corriendo en puerto ${port}`));
+app.listen(process.env.PORT||port,() => console.log(`Servidor corriendo en puerto ${port}`));
 
 app.get('/',(req,res)=> {
     res.sendFile(path.resolve(__dirname,'./views/home.html'));
